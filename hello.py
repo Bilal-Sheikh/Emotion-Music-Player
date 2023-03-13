ce3def18a46e5e477dc55fa891490f59a52ca248
Xapper <72bilal.sheikh@gmail.com>
2023-03-13 19:59:12 +0530

    face cam testing

---
+++ hello.py
@@ -0,0 +1,34 @@
+import cv2
+from deepface import DeepFace
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+web_cam = cv2.VideoCapture(0)
+
+while True:
+    _, frame = web_cam.read()
+
+    to_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(to_gray, 1.1, 4)
+
+    for x,y,w,h in faces:
+        
+        try:
+            detected_emotion = DeepFace.analyze(frame, actions=['emotion'])
+        
+            img = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.putText(frame, detected_emotion[0]['dominant_emotion'], (50, 50), cv2.FONT_HERSHEY_COMPLEX, 3,
+                            (0, 0, 255), 2, cv2.LINE_4)
+
+            print(detected_emotion[0]['dominant_emotion'])
+
+        except:
+            print('no face')
+
+    cv2.imshow('CAM', frame)
+    key = cv2.waitKey(1)
+
+    if key == ord('\x1b'):
+        break
+
+web_cam.release()
