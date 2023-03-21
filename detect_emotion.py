e77e235242becf95599f71cd338fb8ffb948d136
Xapper <72bilal.sheikh@gmail.com>
2023-03-21 18:46:48 +0530

    streamlit changes

--- detect_emotion.py
+++ detect_emotion.py
@@ -1,34 +1,50 @@
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+import av
 import cv2
+import numpy as np
 from deepface import DeepFace
+import webbrowser
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-
-web_cam = cv2.VideoCapture(0)
-
-while True:
-    _, frame = web_cam.read()
-
-    to_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(to_gray, 1.1, 4)
-
-    for x,y,w,h in faces:
+class EmotionProcessor:
+    def recv(self, frm):
         
-        try:
-            detected_emotion = DeepFace.analyze(frame, actions=['emotion'])
+        frame = frm.to_ndarray(format = "bgr24")
         
-            img = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-            cv2.putText(frame, detected_emotion[0]['dominant_emotion'], (50, 50), cv2.FONT_HERSHEY_COMPLEX, 3,
-                            (0, 0, 255), 2, cv2.LINE_4)
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+        to_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(to_gray, 1.1, 4)
 
-            print(detected_emotion[0]['dominant_emotion'])
+        for x,y,w,h in faces:
+            
+            try:
+                detected_emotion = DeepFace.analyze(frame, actions=['emotion'])
+            
+                img = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+                
+                cv2.rectangle(frame, (200, 400), (500, 500), (0, 0, 0), -1)
+                cv2.putText(frame, detected_emotion[0]['dominant_emotion'], (230, 460), cv2.FONT_HERSHEY_COMPLEX, 2,
+                        (0, 255, 0), 2, cv2.LINE_4)
 
-        except:
-            print('no face')
+                print(detected_emotion[0]['dominant_emotion'])
+                np.save("emotion.npy", np.array([detected_emotion[0]['dominant_emotion']]))
 
-    cv2.imshow('CAM', frame)
-    key = cv2.waitKey(1)
+            except:
+                print('no face')
+        
+        return av.VideoFrame.from_ndarray(frame, format = "bgr24")
 
-    if key == ord('\x1b'):
-        break
+try:
+    emotion = np.load("emotion.npy")[0]
+except:
+    emotion = ""
+    
+webrtc_streamer(key = "key", desired_playing_state = True, video_processor_factory = EmotionProcessor)
 
-web_cam.release()
+btn = st.button("Detect my emotions")
+
+if btn:
+    if not(emotion):
+        st.warning("Please show your face correctly")
+    else:
+        webbrowser.open(f"https://www.youtube.com/results?search_query={emotion}+songs")     
--- help.py
+++ help.py
@@ -1,36 +1,37 @@
-import streamlit as st
-from streamlit_webrtc import webrtc_streamer
-import av
 import cv2
-import numpy as np
 from deepface import DeepFace
 
-class EmotionProcessor:
-    def recv(self, frm):
-        
-        frame = frm.to_ndarray(format = "bgr24")
-        
-        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-        to_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(to_gray, 1.1, 4)
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-        for x,y,w,h in faces:
-            
-            try:
-                detected_emotion = DeepFace.analyze(frame, actions=['emotion'])
-            
-                img = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-                cv2.putText(frame, detected_emotion[0]['dominant_emotion'], (50, 50), cv2.FONT_HERSHEY_COMPLEX, 3,
-                                (0, 0, 255), 2, cv2.LINE_4)
+web_cam = cv2.VideoCapture(0)
 
-                print(detected_emotion[0]['dominant_emotion'])
+while True:
+    _, frame = web_cam.read()
 
-            except:
-                print('no face')
-        
-        return av.VideoFrame.from_ndarray(frame, format = "bgr24")
+    to_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(to_gray, 1.1, 4)
 
+    for x, y, w, h in faces:
 
-webrtc_streamer(key = "key", desired_playing_state = True, video_processor_factory = EmotionProcessor)
+        try:
+            detected_emotion = DeepFace.analyze(frame, actions=['emotion'])
 
-btn = st.button("Detect my emotions")
+            img = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+            cv2.rectangle(frame, (200, 400), (500, 500), (0, 0, 0), -1)
+            cv2.putText(frame, detected_emotion[0]['dominant_emotion'], (230, 460), cv2.FONT_HERSHEY_COMPLEX, 2,
+                        (0, 255, 0), 2, cv2.LINE_4)
+
+            print(detected_emotion[0]['dominant_emotion'])
+
+        except:
+            print('no face')
+
+    cv2.imshow('CAM', frame)
+    key = cv2.waitKey(1)
+
+    if key == ord('\x1b'):
+        break
+
+web_cam.release()
